e281e11009f9c1931ba2f68b9ed40c33f686934e
OEURN SEYHA <seyhaoeurn920@gmail.com>
2023-09-01 23:35:35 +0700

    update

--- src/layout/HeaderMobile.js
+++ src/layout/HeaderMobile.js
@@ -1,12 +1,16 @@
 import React from "react";
 import logo from "../image/logo192.png";
+import { Link } from "react-router-dom";
 
 function HeaderMobile() {
   return (
     <div className="bg-gradient-to-r from-cyan-700 to-blue-600 rounded-b-lg shadow-blue-100 shadow-xl">
       <div className="w-full 2xl:w-[1440px] m-auto px-5 py-3 pb-5  text-white flex flex-col gap-5 ">
         <div className=" flex justify-between items-center">
+          <Link to="/">
           <img src={logo} alt="" width="50px" />
+
+          </Link>
           <div className="w-[25%] flex justify-end text-white uppercase font-[500] text-[12px] md:text-[16px]">
             signin | sigup
           </div>
